b574e79f9d603dda65dfc012b7c631d41cd83ec0
Jenim Patel <34350472+JenimPatel@users.noreply.github.com>
2019-02-02 22:53:23 +0530

    add ocr file
    ocr file i.e., optical character recognition file recognizes the alphanumeric characters in the pic and stores as machine 'understandable' format (normal string type)

---
+++ ocr.py
@@ -0,0 +1,22 @@
+from PIL import Image
+import pytesseract
+import argparse
+import cv2
+import os
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True, help="path to input image to be OCR'd")
+ap.add_argument("-p", "--preprocess", type=str, default="thresh", help="type of preprocessing to be done")
+args = vars(ap.parse_args())
+
+image = cv2.imread(args["image"])
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+filename = "{}.png".format(os.getpid())
+
+cv2.imwrite(filename, gray)
+
+text = pytesseract.image_to_string(Image.open(filename))
+print(text)
+
+os.remove(filename)
