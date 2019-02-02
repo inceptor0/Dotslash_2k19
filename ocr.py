f8b5ad09c41e155ee36e48de97ae4c65b54c941d
Jenim Patel <34350472+JenimPatel@users.noreply.github.com>
2019-02-03 04:40:46 +0530

    Update ocr.py

--- ocr.py
+++ ocr.py
@@ -6,7 +6,6 @@ import os
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True, help="path to input image to be OCR'd")
-ap.add_argument("-p", "--preprocess", type=str, default="thresh", help="type of preprocessing to be done")
 args = vars(ap.parse_args())
 
 image = cv2.imread(args["image"])
